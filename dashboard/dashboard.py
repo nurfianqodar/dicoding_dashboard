3e8ea3a26bb49f6277a7de159c2038c80f88f2bc
code-fyn <codefynn77@gmail.com>
2024-08-09 00:29:56 +0700

    import os

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
 
 # Fungsi untuk memuat dan membersihkan dataset
 def load_and_clean_data():
